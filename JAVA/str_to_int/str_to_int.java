3d5ff4feccd731b8f5c1fc3ed707a14bcd7d015d
chrisyang <chrisyang@kkbox.com>
2013-08-01 02:01:02 +0800

    Example of JAVA String to integer and String to Double

---
+++ JAVA/str_to_int/str_to_int.java
@@ -0,0 +1,12 @@
+public class str_to_int {
+    public static void main(String[] args) {
+
+        //parse str "15" to int 15
+        int i1 = Integer.parseInt("15");
+        System.out.println("i1="+i1);
+
+        //parse str 320.31 to double 320.31
+        double d1 = Double.parseDouble("320.31");
+        System.out.println("d1="+d1);
+    }
+}
