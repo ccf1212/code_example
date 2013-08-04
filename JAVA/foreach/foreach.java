c201ec8828c1bfa2650278eb2ea40def960957df
chrisyang <chrisyang@kkbox.com>
2013-08-04 23:01:21 +0800

    Example of JAVA foreach

---
+++ JAVA/foreach/foreach.java
@@ -0,0 +1,9 @@
+public class foreach {
+    public static void main(String[] argv) {
+        int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+
+        for(int num : numbers) {
+            System.out.println("Numbers is: " + num);
+        }
+    }
+}
