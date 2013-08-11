d3b7ae096b6ef4cdb216a2dd6f5523e7e56cdf52
chrisyang <chrisyang@kkbox.com>
2013-08-11 11:05:04 +0800

    PHP regular expression excercise

---
+++ preg.php
@@ -0,0 +1,126 @@
+<?php
+$str2 = <<<DOT
+
+8 / 5 內容新增：小小兵變成貓咪唱香蕉之歌喔>></strong><a target="_blank" href="http://ki07.com/index.php?code=ocBLTNj2HBiOfP6s9_VIKfxgLf4yQxZWKoOVyua5tfJlOk0tC4PKm8dFkAh9nBGyg0rNSDDpDRPM2_eSOLTvI_26Zwu9tgsPYix9IeZ5Uwt6RrWWpRBoB4HLe0c7vSxvBZMmZDJP5c-ftpWkaYpXQFoJTFUQtmAvloHW-Z-AUwOdi3Ztyy222v2EuDFrxwV0cZyofpwbd1fOqjP6fhb_JUCJjDPt3YkbWjYPytbOEPuFBtYKFOqRWD-LgrlGna63jnz-rPf1SCn8YC3-MkMWVtveqSeTTv6zKeRN7lWh06bAiXqNymCAtQM1C-txD7zmBXygbA9miPk="><u>貓咪版香蕉之歌</u></a>
+▼ 就是要利用這個人物的組合來得到特殊禮物。
+<a href="http://cdn.kikinote.com/wp-content/uploads/2013/07/Despicable-Me-1.png"><img src="http://cdn.kikinote.com/wp-content/uploads/2013/07/Despicable-Me-1.png" alt="Despicable Me 1" width="270" height="450" class="alignnone size-full wp-image-5326" /></a>
+<br>
+DOT;
+$str = <<<EOD
+    
+    
+    
+    
+<div style="text-align:center;"> <strong><font size="4" color="#FF0000">教你如何拿高分，金幣破解，提高卑鄙值，服裝獎勵，小小兵貓咪之歌！</font></strong></div>
+<a href="http://www.kikinote.com/wp-content/uploads/2013/06/Home.jpg"><img src="http://www.kikinote.com/wp-content/uploads/2013/06/Home.jpg" alt="Home" width="550" height="300" class="alignnone size-full wp-image-4429" /></a>
+不囉唆！神偷奶爸這個遊戲愈來愈夯囉！加上特有的香蕉之歌在 YouTube 迅速的竄起後，現在小編又整理出來它的影片及遊戲攻略來讓大家人人有功練，分數愈來愈高而且還提供金幣的破解喔！
+8 / 5 內容新增：小小兵變成貓咪唱香蕉之歌喔>></strong><a target="_blank" href="http://ki07.com/index.php?code=ocBLTNj2HBiOfP6s9_VIKfxgLf4yQxZWKoOVyua5tfJlOk0tC4PKm8dFkAh9nBGyg0rNSDDpDRPM2_eSOLTvI_26Zwu9tgsPYix9IeZ5Uwt6RrWWpRBoB4HLe0c7vSxvBZMmZDJP5c-ftpWkaYpXQFoJTFUQtmAvloHW-Z-AUwOdi3Ztyy222v2EuDFrxwV0cZyofpwbd1fOqjP6fhb_JUCJjDPt3YkbWjYPytbOEPuFBtYKFOqRWD-LgrlGna63jnz-rPf1SCn8YC3-MkMWVtveqSeTTv6zKeRN7lWh06bAiXqNymCAtQM1C-txD7zmBXygbA9miPk="><u>貓咪版香蕉之歌</u></a>
+<!--more-->
+<div class="content_bar blue ">重點整理及快速下載點</div>
+<strong>快速下載點：</strong>
+&nbsp;&nbsp;<a href="http://ki07.com/index.php?code=VSr9kx1RIJ3S6jSYCmnozjaK4pafyNcEbEGs_rfRxNN1waDXmYsXZ-DQ9fQOl10SQVhnwc5XEfJEHo2o7Enj7wvTIOY1GnbYC98Ru4Rm8HGLVm6d3B9glaen2ePuJHH_WRB7KCMKqC_q7r11bo_oGng6SbQN-c0mvnudbZ_g7nI8eUAU5A13CPpJiq8vhkZkeybCTdggEt_cDApNAK7btVgE7MIRuQlOU6ce4kTobf-IXo9hjIoqFa1l4lGZ0n2F9I1uZrWfDMjN8e0kKqRo0nDSxChxm-CenhO3epzVWhd6eEIYzS06XWsjIlNfVuzdZfhOJNlwRgt_CamVAuRb1c78HQo2zmDa" target="blank"><u>Android 下載點</u></a>&nbsp;&nbsp;<a href="http://ki07.com/index.php?code=VSr9kx1RIJ3S6jSYCmnozjaK4pafyNcEXZ0Q-tzbISFl_PU5cCv1h9414aZQt1jxENCagx9RvLVq1A6eqqAImWV-cTXIEKpqcP4sl9EjmmkAPK4YMK2QX0_jEn6LTgxc44mDdx-EMPFYQXdPvj9e8mWb334qaNWC2eijgus3mDTknat_QOK3dAzmM3RYLcaS3dnx-qD5ACsPI7EhmUUELMgw-uLCZXMe" target="blank"><u>iOS 下載點</u></a>
+<strong>系統支援：</strong>
+【 Android 】：支援 Android 2.3版本以上。
+【 iOS 】：支援 iOS5.0 以上版本。
+<strong>版本差異：</strong>
+【 免費版 】：目前僅有免費版。
+【 付費版 】：一堆廣告讓你看。
+<strong>內文重點整理：</strong>
+1. 來段香蕉之歌。
+2. 如何拿高分及提高卑鄙值。
+3. 如何獲得服裝獎勵。
+4. 金幣破解方法
+<div class="content_bar blue ">來段香蕉之歌。</div>
+<pre>
+▼ 先來段 High 一點的香蕉之歌。
+<iframe width="600" height="360" src="http://www.youtube.com/embed/KMct-9cJdUg?feature=player_detailpage" frameborder="0" allowfullscreen></iframe>
+<br>
+<strong>香蕉之歌原來還有神人加上字幕>></strong><a target="_blank" href="http://ki07.com/index.php?code=af-qUyFg4z7M2_eSOLTvI_26Zwu9tgsPoKS8TVqfXALq92o-rCvSnOvLB1j9m9cJxxkdcC8RzYfn8qCLk6bVZqMYha-JCzeXWbNBFn9ocMNA_s0ECTTkLv4spMW6ovvTEqFvFh4HIcvZ6KOC6zeYNOSdq39A4rd0LXaqPYH3iRANmFtCA9zkghqsNTXzN7ZdocsxfmkGemWtmT4fExBVsp6gR8xH9xpLyrrHpQSYoweh9RntQRE0yiu6m9WLl1kWzm0ONHwjpAzbbKSPq37S3w=="><u>請看香蕉之歌字幕版</u></a>。
+<strong>小小兵變身貓咪唱香蕉之歌>></strong><a target="_blank" href="http://ki07.com/index.php?code=ocBLTNj2HBiOfP6s9_VIKfxgLf4yQxZWKoOVyua5tfJlOk0tC4PKm8dFkAh9nBGyg0rNSDDpDRPM2_eSOLTvI_26Zwu9tgsPYix9IeZ5Uwt6RrWWpRBoB4HLe0c7vSxvBZMmZDJP5c-ftpWkaYpXQFoJTFUQtmAvloHW-Z-AUwOdi3Ztyy222v2EuDFrxwV0cZyofpwbd1fOqjP6fhb_JUCJjDPt3YkbWjYPytbOEPuFBtYKFOqRWD-LgrlGna63jnz-rPf1SCn8YC3-MkMWVtveqSeTTv6zKeRN7lWh06bAiXqNymCAtQM1C-txD7zmBXygbA9miPk="><u>貓咪版香蕉之歌</u></a>
+</pre>
+<div class="content_bar blue "> 如何拿高分及提高卑鄙值。</div>
+<pre>
+<strong>如何拿高分有四個要點：</strong>
+
+1.<strong>注意卑鄙值要高：卑鄙值是拿高分最重要的因素一定要想辨法提高</strong>
+2.注意前方障礙物：要長眼的看前面的東西，撞昏一次扣分扣很重喔！
+3.打王先閃才攻擊：不要太急的打王，因為你只要閃躲過一次也有加分！
+4.朋友挑戰洗香蕉：這很白話吧！洗香蕉拿高分！
+
+如何提高卑鄙值>><a target="_blank" href="http://ki07.com/index.php?code=Q4h78SVFLDQotJM9gkkoa2-Aun7fh86CMm2Lm8x3uRJPkKI8jSrotZFa0l2ftnXt6a8HiCMyNse3LLvREG-neCetSGm3mWvUxxkdcC8RzYcpQ5reCvsHecFWorwzvG3ikrGI8PT9ZuV7niC-NccNgtvd5T3AHkWlfXG_lpU5jTc_7X_8q4x_s3uWMW6-UvBwekTwE6eb9Cj-eIvt21onxk-Ec_YbWkkUN7Em64Q9xvvGqlVFDiiCqKEfC1IGrdZn0DNvECTblpOh9RntQRE0yiu6m9WLl1kWzm0ONHwjpAzbbKSPq37S3w=="><u>神偷奶爸卑鄙值提高方法</u></a>
+</pre>
+<div class="content_bar blue "> 如何獲得服裝獎勵。</div>
+神偷奶爸這款遊戲，不僅有豐富的道具，還有不同的服裝造型，玩家除了使用香蕉或代幣（花錢購買）獲得外，還可以同過暗碼兌換來免費獲得。以下就讓小編介紹一下如何額外得到禮物。
+<pre>
+▼ 就是要利用這個人物的組合來得到特殊禮物。
+<a href="http://cdn.kikinote.com/wp-content/uploads/2013/07/Despicable-Me-1.png"><img src="http://cdn.kikinote.com/wp-content/uploads/2013/07/Despicable-Me-1.png" alt="Despicable Me 1" width="270" height="450" class="alignnone size-full wp-image-5326" /></a>
+<br>
+▼ 暗碼：功夫嘲諷=消防員+高爾夫+普通小黃人。
+<a href="http://cdn.kikinote.com/wp-content/uploads/2013/07/Despicable-Me-2.png"><img src="http://cdn.kikinote.com/wp-content/uploads/2013/07/Despicable-Me-2.png" alt="Despicable Me 2" width="550" height="350" class="alignnone size-full wp-image-5327" /></a>
+<br>
+▼ X5香蕉特技=草裙舞+鬍子牛仔+消防員。
+<a href="http://cdn.kikinote.com/wp-content/uploads/2013/07/Despicable-Me-3.png"><img src="http://cdn.kikinote.com/wp-content/uploads/2013/07/Despicable-Me-3.png" alt="Despicable Me 3" width="550" height="350" class="alignnone size-full wp-image-5328" /></a>
+<br>
+▼ 大笑嘲諷=高爾夫+嬰兒裝+鬍子牛仔。
+<a href="http://cdn.kikinote.com/wp-content/uploads/2013/07/Despicable-Me-4.png"><img src="http://cdn.kikinote.com/wp-content/uploads/2013/07/Despicable-Me-4.png" alt="Despicable Me 4" width="550" height="350" class="alignnone size-full wp-image-5329" /></a>
+<br>
+▼ 裁判員=嬰兒裝+消防員+草裙舞。
+<a href="http://cdn.kikinote.com/wp-content/uploads/2013/07/Despicable-Me-5.png"><img src="http://cdn.kikinote.com/wp-content/uploads/2013/07/Despicable-Me-5.png" alt="Despicable Me 5" width="550" height="350" class="alignnone size-full wp-image-5330" /></a>
+<br>
+▼ X5分數特技=圍裙裝+草裙舞+嬰兒裝。
+<a href="http://cdn.kikinote.com/wp-content/uploads/2013/07/Despicable-Me-6.png"><img src="http://cdn.kikinote.com/wp-content/uploads/2013/07/Despicable-Me-6.png" alt="Despicable Me 6" width="550" height="350" class="alignnone size-full wp-image-5331" /></a>
+<br>
+</pre>
+<div class="content_bar blue ">金幣破解方法。</div>
+神偷奶爸遊戲中復活都要使用金幣購買，但是一天只有五個免費金幣似乎有點不夠用；所以就有神人用簡單的方法來進行破解囉！不過你的手機要刷機才行加上有個程式八門神器。
+<strong>前往下載</strong>>><a target="_blank" href="http://ki07.com/index.php?code=hyFomggCfrYYrSmVVy2Nfe34wKHKiLpEaTURqlDQKMD-ZNQqUOZciFQLY8u6Rr23b3VM6Qcx6i60kUljL2E1koi5h2Z9QgfZ1CM32oF0JwhGCGCvBQQAOG-HKaVVKwAKxUwMoIDmj82lRUIjVZI8nUOGOGStHbIuFsXKZB-ar5tZ2780q1hZCaH1Ge1BETTKK7qb1YuXWRbObQ40fCOkDNtspI-rftLf"><u>八門神器-game killer</u></a>
+<pre>
+▼ 進遊戲後去購買角色，找最貴的那隻，點下去後會出現你欠多少香蕉。
+<a href="http://cdn.kikinote.com/wp-content/uploads/2013/07/Despicable-Me-7.jpeg"><img src="http://cdn.kikinote.com/wp-content/uploads/2013/07/Despicable-Me-7.jpeg" alt="Despicable Me 7" width="270" height="450" class="alignnone size-full wp-image-5333" /></a>&nbsp;&nbsp;<a href="http://cdn.kikinote.com/wp-content/uploads/2013/07/Despicable-Me-8.jpeg"><img src="http://cdn.kikinote.com/wp-content/uploads/2013/07/Despicable-Me-8.jpeg" alt="Despicable Me 8" width="270" height="450" class="alignnone size-full wp-image-5334" /></a>
+<br>
+▼ 打開八門神器後，選擇 Dword 型
+點擊左下角的白色清單 -> 數據控制 -> 修改所有數據，將數字改-39999。
+之後回到右圖所示的，那個缺多少香蕉那裡，點下去！
+<a href="http://cdn.kikinote.com/wp-content/uploads/2013/07/Despicable-Me-9.png"><img src="http://cdn.kikinote.com/wp-content/uploads/2013/07/Despicable-Me-9.png" alt="Despicable Me 9" width="270" height="450" class="alignnone size-full wp-image-5335" /></a>&nbsp;&nbsp;<a href="http://cdn.kikinote.com/wp-content/uploads/2013/07/Despicable-Me-10.jpeg"><img src="http://cdn.kikinote.com/wp-content/uploads/2013/07/Despicable-Me-10.jpeg" alt="Despicable Me 10" width="270" height="450" class="alignnone size-full wp-image-5336" /></a>
+<br>
+▼ 哈哈錢變多囉。
+<a href="http://cdn.kikinote.com/wp-content/uploads/2013/07/Despicable-Me-11.jpeg"><img src="http://cdn.kikinote.com/wp-content/uploads/2013/07/Despicable-Me-11.jpeg" alt="Despicable Me 11" width="270" height="450" class="alignnone size-full wp-image-5337" /></a>
+<br>
+</pre>
+<span style="color: #c0c0c0;">圖文資料參考取自：18touch 及遊戲魔人及 Steven5538 由 kikinote 匯整提供。</span>
+前往下載</strong>>><a target="_blank" href="http://ki07.com/index.php?code=hyFomggCfrYYrSmVVy2Nfe34wKHKiLpEaTURqlDQKMD-ZNQqUOZciFQLY8u6Rr23b3VM6Qcx6i60kUljL2E1koi5h2Z9QgfZ1CM32oF0JwhGCGCvBQQAOG-HKaVVKwAKxUwMoIDmj82lRUIjVZI8nUOGOGStHbIuFsXKZB-ar5tZ2780q1hZCaH1Ge1BETTKK7qb1YuXWRbObQ40fCOkDNtspI-rftLf"><u>無限多的金幣囉！</u></a>
+EOD;
+
+$str4 = $str2;
+$str2 = $str;
+$str5 = $str4;
+$text = strip_tags($str);
+//echo $text;
+while(preg_match("/(?<!a href=\")(?<!src=\")((http|ftp)+(s)?:\/\/[^<>\s]+)/i", $str2)) {
+    $str2= preg_replace(
+        "/(?<!a href=\")(?<!src=\")((http|ftp)+(s)?:\/\/[^<>\s]+)/i",
+        "",
+        $str2
+    );
+}
+while(preg_match("/<img[^>]*src *= *[\"']?([^\"']*)/i", $str2)) {
+    $str2= preg_replace(
+        "/<img[^>]*src *= *[\"']?([^\"']*)/i",
+        "",
+        $str2
+    );
+}
+/*
+$str3= preg_replace(
+    "/<img[^>]*src *= *[\"']?([^\"']*)/i",
+    "",
+    $str3
+);
+*/
+//echo "<PRE>";
+//$text = trim($text);
+echo $text . "\n";
+//echo $str4;
+//echo "</PRE>";
+//echo $str;
+?>
