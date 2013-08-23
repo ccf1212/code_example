9ff886ae199b198fd28e897726598fdf72f4231e
chrisyang <chrisyang@kkbox.com>
2013-08-24 00:38:06 +0800

    Example of PHP implode and explode

---
+++ PHP/explode.php
@@ -0,0 +1,9 @@
+<?php
+//A string with some terms
+$str = "Hold,This,Cat,HTC";
+
+//split terms into array
+$array = explode(",", $str);
+
+//print the result
+print_r($array);
---
+++ PHP/implode.php
@@ -0,0 +1,10 @@
+<?php
+//Announce a array of string
+$array = array('I', 'am', 'ironman');
+
+//seperate every elemment with comma ','
+$str = implode(',', $array);
+
+//print the result
+echo $str;
+
